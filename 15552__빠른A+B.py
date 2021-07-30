33f4858905cf218883dc7568da322ed8ee8bc7f0
parksu11111 <63779320+parksu11111@users.noreply.github.com>
2021-07-30 23:18:01 +0900

    Update 15552__빠른A+B.py

--- 15552__빠른A+B.py
+++ 15552__빠른A+B.py
@@ -1,5 +1,5 @@
-import sys        '//sys 정의 (sys.stdin.readline()을 사용하기 위헤)
-T = int(sys.stdin.readline())  '//input() 대신 sys.stdin.readline() 을 사용할 수 있다. 하지만 이때는 \n과 같은게 따라온다. 그래서 int 로 변환하여 없앤다. 또는 뒤에 .rstrip()룰 추가한다.
+import sys        'sys 정의 (sys.stdin.readline()을 사용하기 위헤)
+T = int(sys.stdin.readline())  'input() 대신 sys.stdin.readline() 을 사용할 수 있다. 하지만 이때는 \n과 같은게 따라온다. 그래서 int 로 변환하여 없앤다. 또는 뒤에 .rstrip()룰 추가한다.
 
 for i in range(T):
     a,b=  map (int,sys.stdin.readline().split())
