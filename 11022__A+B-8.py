943086b528661bb3dc7dc43af4559ed8e11fdf1c
parksu11111 <63779320+parksu11111@users.noreply.github.com>
2021-08-06 16:13:42 +0900

    Create 11022__A+B-8.py

---
+++ 11022__A+B-8.py
@@ -0,0 +1,11 @@
+T = input()
+
+for i in range(1,int(T)+1):  //  1부터 T까지
+    A,B = map(int,input().split())
+    print("Case #{}: {} + {} = {}".format(i,A,B,A+B))
+
+    
+    
+    *range(A) // 0부터 A-1까지 
+    *range(A,B) // A부터 B-1까지
+    
