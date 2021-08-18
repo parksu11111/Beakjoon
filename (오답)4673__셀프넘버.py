aa6891d3591e3fa4058d75a20533eadfb595aacf
parksu11111 <63779320+parksu11111@users.noreply.github.com>
2021-08-18 23:49:04 +0900

    Create (오답)4673__셀프넘버.py

---
+++ (오답)4673__셀프넘버.py
@@ -0,0 +1,18 @@
+
+'퍼 온 코드
+
+natural_num = set(range(1, 10001))
+generated_num = set()
+
+for i in range(1, 10001):       # i = 850       
+    for j in str(i):            # j = "8", "5", "0"
+        i += int(j)             # 850 + 8 + 5 + 0, i = 863
+    generated_num.add(i)        # 생성자가 있는 숫자들
+
+self_num = sorted(natural_num - generated_num)
+for i in self_num:
+    print(i)
+    
+    'sorted(숫자들 or 문자들) : 순서대로 정렬해줌 ---->반환타임: list
+    
+    
