9ae12ea3c7a7729f70c9c83b87361f9dbcfba7fc
parksu11111 <63779320+parksu11111@users.noreply.github.com>
2021-08-20 17:24:21 +0900

    Create 11654__아스키코드.py

---
+++ 11654__아스키코드.py
@@ -0,0 +1,15 @@
+AA= input()
+        
+if ord(AA)>=65 and ord(AA)<=90:
+    print(ord (AA))
+
+if ord(AA)>=97 and ord(AA)<=122:
+    print(ord(AA))
+
+if ord(AA)>=48 and ord(AA)<=57:
+    print(ord(AA))
+
+    
+    
+    '문자,숫자 ---->  아스키코드  : ord( '문자' or '숫자')   'ex) ord('A') = 65
+    '아스키코드 -----> 문자,숫자  : chr('아스키코드)       ex) chr(65) = A
