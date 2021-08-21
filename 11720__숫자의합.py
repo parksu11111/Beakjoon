ed8ab0f3935e27a7ddf979ceedae6407c8dd2d15
parksu11111 <63779320+parksu11111@users.noreply.github.com>
2021-08-21 22:27:34 +0900

    Create 11720__숫자의합.py

---
+++ 11720__숫자의합.py
@@ -0,0 +1,25 @@
+N=int(input())
+numbers = input()
+result=0
+for i in range(N):
+    result += int(a[i])
+print(result)
+
+
+
+
+
+
+'다른풀이1
+n = input()
+print(sum(map(int,input()))) 
+
+
+
+'다른풀이2
+n = input()
+numnbers = input()
+resilt = 0
+for i in numbers :
+    result += int(i)  # result= result+int(i)
+print(result)
